312eec75e502a233c7d0c1925626aa33ff571975
Aram Hamo <contact@aramhamo.me>
2023-03-25 15:20:14 +0100

    new file:   .config.php 	modified:   Mvc/DB.php 	modified:   mvc

---
+++ .config.php
@@ -0,0 +1,17 @@
+<?php declare(strict_types=1);
+define("CONFIG",array(
+/*
+  "DATABASE"=>array(
+    "SOFTWARE"=>"mysql",
+    "DATABASE"=>"exampleDB",
+    "USERNAME"=>"root",
+    "PASSWORD"=>"",
+    "HOST"=>""
+  ),
+*/
+  "DATABASE"=>array(
+    "SOFTWARE"=>"sqlite",
+    "PATH"=>"db.sqlite"
+  ),
+  "DEBUG_MODE"=>true
+));
--- Mvc/DB.php
+++ Mvc/DB.php
@@ -1,15 +1,23 @@
 <?php declare(strict_types=1);
 namespace AramHamo\Mvc;
 use \PDO;
+class DB {
+  public $conn;
+  function __construct(){
 
-class DB{
-    public $conn;
-    function __construct(){
-      try{
-        $this->conn = new \PDO("sqlite:/".__DIR__."/../db.sqlite");
-        $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-      }catch(Exception $e){
-        echo $e;
-      }
+    switch(CONFIG["DATABASE"]["SOFTWARE"]){
+      case "mysql":
+        $GLOBALS["dsn"] = "mysql:host=". CONFIG["DATABASE"]["HOST"].";dbname=". CONFIG["DATABASE"]["DATABASE"];
+      break;
+      case "sqlite":
+        $GLOBALS["dsn"] = "sqlite:/".__DIR__."/../". CONFIG["DATABASE"]["PATH"];
+      break;
     }
+    try{
+      $this->conn = new \PDO($GLOBALS["dsn"]);
+      $this->conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    }catch(Exception $e){
+      echo $e;
+    }
+  }
 }
