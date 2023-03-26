bba546cee20bcd853866d9886444136961e4cd4f
Aram Hamo <contact@aramhamo.me>
2023-03-26 09:19:13 +0200

    modified:   Mvc/Migration.php

--- Mvc/Migration.php
+++ Mvc/Migration.php
@@ -4,11 +4,11 @@ namespace AramHamo\Mvc;
 class Migration{
   public $table = [];
   public $options = '';
-  public function id(){
+  public function id():Object{
     $this->table["id"] = "id INTEGER PRIMARY KEY AUTOINCREMENT";
   }
 
-  public function text(String $attr,int $length=0){
+  public function text(String $attr,int $length=0):Object{
     if($length < 1){
       $this->table["$attr"] = "$attr TEXT";
     }else{
@@ -17,15 +17,15 @@ class Migration{
     return $this;
   }
 
-  public function char(String $attr,int $length=0){
+  public function char(String $attr,int $length=0):Object{
     return $this->text($attr,$length);
   }
 
-  public function varchar(String $attr,int $length=0){
+  public function varchar(String $attr,int $length=0):Object{
     return $this->text($attr,$length);
   }
 
-  public function int(String $attr,int $length){
+  public function int(String $attr,int $length):Object{
     if($length < 1){
       $this->table[$attr] = "$attr INT";
     }else{
@@ -34,27 +34,27 @@ class Migration{
     return $this;
   }
 
-  public function bool(String $attr){
+  public function bool(String $attr):Object{
     $this->table[$attr] = "$attr BOOLEAN";
     return $this;
   }
 
-  public function blob(String $attr){
+  public function blob(String $attr):Object{
     $this->table[$attr] = "$attr BLOB";
     return $this;
   }
 
-  public function primaryKey($attr){
+  public function primaryKey($attr):Object{
     $this->options .= ",PRIMARY KEY($attr)";
     return $this;
   }
 
-  public function unique($attr){
+  public function unique($attr):Object{
     $this->options .= ",UNIQUE($attr)";
     return $this;
   }
 
-  public function foreignKey(String $attr,String $table,String $tableAttr){
+  public function foreignKey(String $attr,String $table,String $tableAttr):Object{
     $this->options .= ",FOREIGN KEY($attr) REFERENCES $table($tableAttr)";
     return $this;
   }
