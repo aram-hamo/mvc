d25b9cb5ee3e340aa1effb70903265dd0dd1ece8
Aram Hamo <contact@aramhamo.me>
2023-04-01 22:32:26 +0200

    modified:   public/index.php

--- public/index.php
+++ public/index.php
@@ -4,37 +4,36 @@ define("CONFIG",parse_ini_file('../.env',true));
 use AramHamo\MvcCore\Router;
 
 $router = new Router;
-$_SERVER['SERVER_SOFTWARE'] = $_SERVER['SERVER_SOFTWARE'] ?? '';
-define('SERVERSOFTWARE',$_SERVER['SERVER_SOFTWARE']);
 
-if(CONFIG["GENERAL"]["FORCE_SSL"] && $_SERVER["SERVER_SOFTWARE"] !== '' && empty($_SERVER["HTTPS"])){
+if(CONFIG["GENERAL"]["FORCE_SSL"] && php_sapi_name() !== 'cli' && empty($_SERVER["HTTPS"])){
   $_SERVER["HTTP_HOST"] = $_SERVER["HTTP_HOST"] ?? '';
   $_SERVER["REQUEST_URI"] = $_SERVER["REQUEST_URI"] ?? '';
   header("HTTP/1.1 301 Moved Permanently");
   header("Location: "."https://" . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"] );
   exit;
 }
-
-if(!empty(SERVERSOFTWARE)){
-  if(strpos(SERVERSOFTWARE,'PHP') !== false){
-    $SERVER_SOFTWWARE = "php";
+switch(php_sapi_name()){
+  case "cli-server":
     if(isset($_SERVER['PATH_INFO'])){
       $router->listen($_SERVER['PATH_INFO']);
     }else{
       $router->listen('');
     }
-  }else if(strpos(SERVERSOFTWARE,'Apache') !== false){
-    $SERVER_SOFTWWARE = "apache";
+  break;
+  case "apache2handler":
     if(isset($_GET['url'])){
       $router->listen($_GET['url']);
     }else{
       $router->listen('');
     }
-  }
-}else{
-  if(isset($argv[1])){
-    $router->listen($argv[1]);
-  }else{
-    $router->listen('');
-  }
+  break;
+  case "cli":
+    if(isset($argv[1])){
+      $router->listen($argv[1]);
+    }else{
+      $router->listen('');
+    }
+  break;
+  default:
+    echo "server software not supported";
 }
