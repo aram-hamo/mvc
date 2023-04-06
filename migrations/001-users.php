7665360aceb8a8f82ae02b6f026c6a48f2b679a0
Aram Hamo <contact@aramhamo.me>
2023-04-06 17:59:36 +0200

    new file:   migrations/001-users.php new file:   models/User.php

---
+++ migrations/001-users.php
@@ -0,0 +1,23 @@
+<?php
+use AramHamo\MvcCore\Migration;
+use AramHamo\MvcCore\Schema;
+$GLOBALS['tableName'] = "users";
+$table = new class {
+  static function up(){
+    $s = new Schema;
+    $m = new Migration;
+    $m->id();
+    $m->text("firstName");
+    $m->text("lastName");
+    $m->text("username")->unique("username");
+    $m->text("password");
+    $m->text("email");
+    $m->bool("verified");
+    $m->text("tokan")->unique("tokan");
+
+    $s->create($GLOBALS['tableName'],$m->table,$m->options);
+  }
+  static function down(){
+    $this->dropIfExists($GLOBALS['tableName']);
+  }
+};
